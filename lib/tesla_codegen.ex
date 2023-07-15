3f10a67e27683836b5b87cc0cda00de31f33c6e5
Nelson Estevão <nelson@marmelasoft.com>
2023-07-15 12:24:08 +0100

    🌅

---
+++ lib/tesla_codegen.ex
@@ -0,0 +1,18 @@
+defmodule TeslaCodegen do
+  @moduledoc """
+  Documentation for `TeslaCodegen`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TeslaCodegen.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tesla_codegen_test.exs
@@ -0,0 +1,8 @@
+defmodule TeslaCodegenTest do
+  use ExUnit.Case
+  doctest TeslaCodegen
+
+  test "greets the world" do
+    assert TeslaCodegen.hello() == :world
+  end
+end
