9d14d251604646c681bf1db3c9e826a2089adadb
Nelson Estevão <nelson@marmelasoft.com>
2023-07-15 13:53:25 +0100

    📁 Add OpenAPI 3.0 Petstore example for testing purposes

--- mix.exs
+++ mix.exs
@@ -1,28 +1,55 @@
 defmodule TeslaCodegen.MixProject do
   use Mix.Project
 
+  @app :tesla_codegen
+  @version "0.0.0"
+
   def project do
     [
-      app: :tesla_codegen,
-      version: "0.1.0",
+      app: @app,
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      dialyzer: [plt_add_apps: [:ex_unit]],
+      preferred_cli_env: [
+        check: :test
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    [extra_applications: [:logger]]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # parsers
+      {:jason, "~> 1.4"},
+
+      # tools
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.30", only: :dev, runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get"],
+      "lint.credo": ["credo --strict --all"],
+      "lint.dialyzer": ["dialyzer --format dialyxir"],
+      lint: ["lint.dialyzer", "lint.credo"],
+      check: [
+        "clean",
+        "deps.unlock --check-unused",
+        "compile --warnings-as-errors",
+        "format --check-formatted",
+        "deps.unlock --check-unused",
+        "test --warnings-as-errors",
+        "lint"
+      ]
     ]
   end
 end
