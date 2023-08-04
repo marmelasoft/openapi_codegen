c9fe3c9295a7bfb6ddaade6e989c326e1f8b3821
Nelson Estevão <nelson@marmelasoft.com>
2023-08-04 23:29:00 +0100

    chore: bring CI back, upgrade deps, improve code (#6)

--- lib/tesla_codegen/ast.ex
+++ lib/tesla_codegen/ast.ex
@@ -1,13 +1,13 @@
 defmodule TeslaCodegen.Ast do
   @moduledoc """
-  AST operations
+  AST operations.
   """
 
   @doc """
-  Converts the AST into a string, formats it, styles it and writes it to a file and returns the path to the file
+  Converts the AST into a string, formats it, styles it and writes it to a file and returns the path to the file.
   """
-  @spec ast_to_file!(Macro.t(), String.t(), Path.t()) :: Path.t()
-  def ast_to_file!(ast, key, path) do
+  @spec to_file!(Macro.t(), String.t(), Path.t()) :: Path.t()
+  def to_file!(ast, key, path) do
     File.mkdir_p!(path)
 
     file_path = Path.join(path, "#{key}.ex")
@@ -23,7 +23,18 @@ defmodule TeslaCodegen.Ast do
   end
 
   @doc """
-  Converts a string or atom into a variable
+  Converts a string or atom into a variable.
+
+  ## Examples
+
+      iex> to_var(:my_var, __MODULE__)
+      {:my_var, [], __MODULE__}
+
+      iex> to_var("my_var", __MODULE__)
+      {:my_var, [], __MODULE__}
+
+      iex> to_var("myVar", __MODULE__)
+      {:my_var, [], __MODULE__}
   """
   @spec to_var(binary() | atom(), atom()) :: Macro.t()
   def to_var(name, context) when is_atom(name) do
@@ -32,7 +43,7 @@ defmodule TeslaCodegen.Ast do
     |> to_var(context)
   end
 
-  def to_var(name, context) do
+  def to_var(name, context) when is_binary(name) do
     name
     |> Macro.underscore()
     |> String.to_atom()
