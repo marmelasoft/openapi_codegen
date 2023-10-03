87b023f2e88426990a5e8e195b6cbfe5688793b6
Filipe Cabaço <filipecabaco@gmail.com>
2023-10-03 18:55:33 +0100

    Minor refactor to remove Path building code

--- test/tesla_codegen/ast_test.exs
+++ test/tesla_codegen/ast_test.exs
@@ -1,5 +1,21 @@
 defmodule OpenApiCodegen.AstTest do
   use ExUnit.Case
 
-  doctest OpenApiCodegen.Ast, import: true
+  alias OpenApiCodegen.Ast
+
+  doctest Ast, import: true
+
+  describe "generate_path_interpolation/2" do
+    test "with URL params" do
+      assert __MODULE__
+             |> Ast.generate_path_interpolation("/users/{user_id}/posts/{post_id}")
+             |> Macro.to_string() == "\"/users/\#{user_id}/posts/\#{post_id}\""
+    end
+
+    test "without URL params" do
+      assert __MODULE__
+             |> Ast.generate_path_interpolation("/users/1/posts/2")
+             |> Macro.to_string() == "\"/users/1/posts/2\""
+    end
+  end
 end
